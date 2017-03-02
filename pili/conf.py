429a1f21fbb91f2a88471f7fbd82c1a0973b7562
wangtuanjie <wangtuanjiezh@gmail.com>
2017-03-02 14:58:46 +0800

    useragent

--- pili/conf.py
+++ pili/conf.py
@@ -1,5 +1,11 @@
 """
 Settings
 """
+import platform
+import sys
+
+
 API_VERSION = 'v2'
 API_HOST = 'pili.qiniuapi.com'
+
+API_USERAGENT = "pili-sdk-python/v2 %s %s" % (platform.python_version(), sys.platform)
