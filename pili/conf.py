91019eeb4ec1175af20fd85ef3dd2c97942553b3
Anonymous <anonymous@Anonymous.local>
2017-04-25 14:35:05 +0800

    add RTC

--- pili/conf.py
+++ pili/conf.py
@@ -7,5 +7,6 @@ import sys
 
 API_VERSION = 'v2'
 API_HOST = 'pili.qiniuapi.com'
-
 API_USERAGENT = "pili-sdk-python/v2 %s %s" % (platform.python_version(), sys.platform)
+RTC_API_VERSION = 'v1'
+RTC_API_HOST = 'rtc.qiniuapi.com'
